2fa98d70ad5b8070d5481a725d8278a6c73669b0
0xkiichiro <0xkiichiro@proton.me>
2023-09-18 17:18:55 +0300

    print person data to formatted pdf, generate pdf with person name

--- core.py
+++ core.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 
 class LinkedinScaper:
     def __init__(self):
@@ -25,7 +27,7 @@ class LinkedinScaper:
         password_input.send_keys(Keys.ENTER)
         # time.sleep(15) # fix later, this is a hack to manually surpass captcha
         time.sleep(self.WAIT_TIME)
-        print("logged in to account")
+        print("logged into account")
 
     def go_to_profile(self, profile_name):
         self.driver.get(f"http://www.linkedin.com/in/{profile_name}")
@@ -52,27 +54,27 @@ class LinkedinScaper:
             try:
                 experience_info["title"] = experience.find_element(By.XPATH, ".//div/div[2]/div[1]/div[1]/div/div/div/div/span[1]").text
             except:
-                experience_info["title"] = "No title"
+                experience_info["title"] = None
 
             try:
                 experience_info["company_name_and_contract_type"] = experience.find_element(By.XPATH, ".//div/div[2]/div[1]/div[1]/span[1]/span[1]").text
             except:
-                experience_info["company_name_and_contract_type"] = "No company name | contract type"
+                experience_info["company_name_and_contract_type"] = None
 
             try:
                 experience_info["dates"] = experience.find_element(By.XPATH, ".//div/div[2]/div[1]/div[1]/span[2]/span[1]").text
             except:
-                experience_info["dates"] = "No dates"
+                experience_info["dates"] = None
 
             try:
                 experience_info["job_description"] = experience.find_element(By.XPATH, ".//div/div[2]/div[2]/ul/li[1]/div/ul/li/div/div/div/div/span[1]").text
             except:
-                experience_info["job_description"] = f"No job desc for {experience_info['company_name_and_contract_type']}"
+                experience_info["job_description"] = None
 
             try:
                 experience_info["skills"] = experience.find_element(By.XPATH, ".//div/div[2]/div[2]/ul/li[2]/div/ul/li/div/div/div/div/span[1]").text
             except:
-                experience_info["skills"] = f"No skills for {experience_info['company_name_and_contract_type']}"
+                experience_info["skills"] = None
 
             person.experiences.append(experience_info)
         
@@ -87,17 +89,17 @@ class LinkedinScaper:
             try:
                 education_info["school_name"] = education.find_element(By.XPATH, ".//div/div[2]/div/a/div/div/div/div/span[1]").text
             except:
-                education_info["school_name"] = "------"
+                education_info["school_name"] = None
 
             try:
                 education_info["degree_type"] = education.find_element(By.XPATH, ".//div/div[2]/div/a/span[1]/span[1]").text
             except:
-                education_info["degree_type"] = "------"
+                education_info["degree_type"] = None
 
             try:
                 education_info["school_years"] = education.find_element(By.XPATH, ".//div/div[2]/div/a/span[2]/span[1]").text
             except:
-                education_info["school_years"] = "------"
+                education_info["school_years"] = None
 
             person.education.append(education_info)
         
@@ -107,6 +109,7 @@ class LinkedinScaper:
         print("Location:", person.location)
         print("Experiences:", person.experiences)
         print("Education:", person.education)
+
         return person
 
     def close_browser(self):
@@ -119,3 +122,90 @@ class Person:
         self.location = ""
         self.experiences = []
         self.education = []
+
+class PDFGenerator:
+    def generate_pdf(self, person, pdf_filename):
+        print(f"generating pdf for {person.name}")
+        # Create a PDF file
+        c = canvas.Canvas(pdf_filename, pagesize=letter)
+
+        # Add heading
+        c.setFont("Helvetica-Bold", 14)
+        c.drawString(100, 725, "Curriculum Vitae")
+
+        # Add sub-heading
+        c.setFont("Helvetica-Bold", 12)
+        c.drawString(120, 700, "Personal Information")
+        c.setFont("Helvetica", 10)
+        # Write the person's information to the PDF
+        c.drawString(140, 680, person.name)
+        c.drawString(140, 660, person.title)
+        c.drawString(140, 640, person.location)
+
+        # Experiences
+        c.setFont("Helvetica-Bold", 12)
+        c.drawString(120, 620, "Experiences:")
+        c.setFont("Helvetica", 12)
+        y = 600
+        for experience in person.experiences:
+            if experience["title"] is None and experience["company_name_and_contract_type"] is None and experience["dates"] is None and experience["job_description"] is None and experience["skills"] is None:
+                pass
+            else:
+                if experience["title"] is not None:
+                    c.setFillColorRGB(0, 0, 0, 1)
+                    c.setFont("Helvetica-Bold", 10)
+                    c.drawString(140, y, experience["title"])
+                    y -= 20
+                if experience["company_name_and_contract_type"] is not None:
+                    c.setFillColorRGB(0, 0, 0, 1)
+                    c.setFont("Helvetica", 10)
+                    c.drawString(140, y, experience["company_name_and_contract_type"])
+                    y -= 20
+                if experience["dates"] is not None:
+                    c.setFillColorRGB(0, 0, 0, 0.6)
+                    c.setFont("Helvetica-Oblique", 8)
+                    c.drawString(140, y, experience["dates"])
+                    y -= 20
+                if experience["job_description"] is not None:
+                    c.setFillColorRGB(0, 0, 0, 1)
+                    c.setFont("Helvetica", 8)
+                    job_description_lines = experience["job_description"].split('\n')
+                    for line in job_description_lines:
+                        c.drawString(140, y, line)
+                        y -= 20
+                if experience["skills"] is not None:
+                    c.setFillColorRGB(0, 0, 0, 1)
+                    c.setFont("Helvetica", 8)
+                    c.drawString(140, y, experience["skills"])
+                    y -= 20
+
+        # Education
+        c.setFillColorRGB(0, 0, 0, 1)
+        c.setFont("Helvetica-Bold", 12)
+        c.drawString(120, y - 20, "Education:")
+        c.setFont("Helvetica", 10)
+        y -= 40
+        for education in person.education:
+            if education["school_name"] is None and education["degree_type"] is None and education["school_years"] is None:
+                pass
+            else:
+                if education["school_name"] is not None:
+                    c.setFillColorRGB(0, 0, 0, 1)
+                    c.setFont("Helvetica-Bold", 10)
+                    c.drawString(140, y, education["school_name"])
+                    y -= 20
+                if education["degree_type"] is not None:
+                    c.setFillColorRGB(0, 0, 0, 1)
+                    c.setFont("Helvetica", 8)
+                    c.drawString(140, y, education["degree_type"])
+                    y -= 20
+                if education["school_years"] is not None:
+                    c.setFillColorRGB(0, 0, 0, 0.6)
+                    c.setFont("Helvetica-Oblique", 8)
+                    c.drawString(140, y, education["school_years"])
+                    y -= 20
+
+
+        # Save the PDF file
+        c.save()
+        print(f"PDF file '{pdf_filename}' created successfully.")
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from core import LinkedinScaper
+from core import PDFGenerator
 import json
 
 # init scraper
@@ -8,7 +9,13 @@ with open("userdata.json", "r") as userdata:
     userdata = json.load(userdata)
 # login
 liscraper.login(userdata["email"], userdata["password"])
+
 #starting to scrape
-liscraper.scrape_person("alkan-kosar")
-liscraper.scrape_person("batu-buktel")
-liscraper.scrape_person("alper-batuhan-685272260")
+scrape_list = ["alkan-kosar", "batu-buktel", "alper-batuhan-685272260", "barisaytimur", "cem-sarı-729b371b5", "cansu-özer-180003214"]
+for scrapee in scrape_list:
+    person = liscraper.scrape_person(scrapee)
+    pdf_generator = PDFGenerator()
+    pdf_generator.generate_pdf(person, f"{scrapee}.pdf")
+
+
+liscraper.close_browser()
