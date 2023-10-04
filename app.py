e9765f15a4754abbed90bf6c055efa13c6c558c3
0xkiichiro <0xkiichiro@proton.me>
2023-10-04 13:42:25 +0300

    setup error logging and restart on error

--- app.py
+++ app.py
@@ -2,21 +2,27 @@ from core import LinkedinScaper
 from core import PDFGenerator
 import json
 import os
+import logging
 from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 
+# Set up logging
+logging.basicConfig(filename='app.log', level=logging.ERROR, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Set up CORS for cross origin information transfer
 app = Flask(__name__, template_folder='.')
 CORS(app)
-# todo: move scraper init above function to avoid multiple browser instances & improve speed
+
+# init scraper
+liscraper = LinkedinScaper()
+# read userdata from json
+with open("userdata.json", "r") as userdata:
+    userdata = json.load(userdata)
+# login
+liscraper.login(userdata[3]["email"], userdata[3]["password"])
+
 @app.route("/scrape/<linkedin_handle>", methods=["GET"])
 def scrape_person_data(linkedin_handle: str):
-    # init scraper
-    liscraper = LinkedinScaper()
-    # read userdata from json
-    with open("userdata.json", "r") as userdata:
-        userdata = json.load(userdata)
-    # login
-    liscraper.login(userdata[3]["email"], userdata[3]["password"])
     person_data = liscraper.scrape_person(linkedin_handle)
     if person_data:
         return jsonify(person_data)  # Return the JSON-serializable dictionary
@@ -44,5 +50,12 @@ def generate_pdf(linkedin_handle: str):
     except Exception as e:
         return str(e), 500  # Handle any exceptions that may occur during PDF generation
 
+def run_app():
+    try:
+        app.run(debug=True, host='0.0.0.0', port=5001)
+    except Exception as e:
+        logging.error(f"Flask app crashed: {e}")
+        run_app()  # Restart the app
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    run_app()
